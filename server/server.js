a717533dc784169ed2cbc84e9e901be903ff1e99
Frane Anđelić <103383776+Frane-Bog@users.noreply.github.com>
2023-04-18 09:09:38 +0000

    Promjena strutkure koda

--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-express()
-    .get('/', function (req, res) { return res.send('Frane'); })
-    .listen(3000, function () { return console.log('Server is running get'); });
-express().post("*", function (req, res) {
-    console.log("POST", req.body);
-    res.send("POST");
-}).listen(4000, function () { return console.log("Server is running post"); });
